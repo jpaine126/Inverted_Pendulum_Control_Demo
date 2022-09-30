19615946572d703467c185991c994409c9155b37
jpaine126 <41339610+jpaine126@users.noreply.github.com>
2022-09-30 14:49:29 -0400

    black

--- LQR.py
+++ LQR.py
@@ -15,33 +15,33 @@ This class if for implementing a LQR controller, not for designing one.
 
 from math import copysign
 
-class LQR:
 
+class LQR:
     def __init__(self, K, max_input=1, max_input_on=False):
         self.K = K
         self.max_input = max_input
         self.max_input_on = max_input_on
-        
+
     def set_gain(self, K):
         self.K = K
-        
+
     def max_input_enable(self):
         self.max_input_on = True
-    
+
     def max_input_disable(self):
         self.max_input_on = False
-        
+
     def calc_force(self, states):
-      
+
         control = 0
-        
+
         for i in range(0, len(states)):
-            control += states[i,0]*self.K[0,i]
-            #print(states[i,0])
-            #print(self.K[0,1])
-            
+            control += states[i, 0] * self.K[0, i]
+            # print(states[i,0])
+            # print(self.K[0,1])
+
         if self.max_input_on:
             if abs(control) > self.max_input:
                 control = copysign(self.max_input, control)
-            
+
         return -control
--- PID.py
+++ PID.py
@@ -21,77 +21,59 @@ from math import copysign
 
 
 class PID:
-    
-
     def __init__(self, K_P, K_I, K_D, dt=1, antiwindupmode=0, control_limit=10):
         self.K_P = K_P
         self.K_I = K_I
         self.K_D = K_D
-        self.dt  = dt
+        self.dt = dt
         self.last_error = 0
         self.integrator = 0
         self.control_limit = control_limit
         self.antiwindup_mode = antiwindupmode
-        
+
     " Getters and Setters "
-        
+
     def set_kp(self, K_P):
-        self.K_P = K_P 
+        self.K_P = K_P
+
     def set_ki(self, K_I):
         self.K_I = K_I
+
     def set_kd(self, K_D):
         self.K_D = K_D
-        
+
     def set_dt(self, dt):
         self.dt = dt
-        
+
     def set_last_error(self, last_error):
         self.last_error = last_error
-        
+
     def set_integrator(self, integrator):
         self.integrator = integrator
-        
+
     def set_control_limit(self, control_limit):
         self.control_limit = control_limit
-        
+
     def set_antiwindup_mode(self, antiwindup_mode):
         self.antiwindup_mode = antiwindup_mode
-        
-    " Utility Functions "
-    
 
-    
+    " Utility Functions "
+
     " Control Force Update Function "
-        
+
     def update(self, error):
-        integral = self.integrator + error*self.dt
-        deriv = (error - self.last_error)/self.dt
-        
+        integral = self.integrator + error * self.dt
+        deriv = (error - self.last_error) / self.dt
+
         if self.antiwindup_mode == 0:
-            control = error*self.K_P + integral*self.K_I + deriv*self.K_D
-        
+            control = error * self.K_P + integral * self.K_I + deriv * self.K_D
+
         elif self.antiwindup_mode == 1:
             if abs(integral) > self.control_limit:
                 integral = copysign(self.control_limit, integral)
-            control = error*self.K_P + integral*self.K_I + deriv*self.K_D
-           
+            control = error * self.K_P + integral * self.K_I + deriv * self.K_D
+
         self.last_error = error
         self.integrator = integral
-        
+
         return control
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
