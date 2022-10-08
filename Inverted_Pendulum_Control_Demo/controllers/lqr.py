4cd789b57e9ba2920e79299832a755fbd642d4c2
jpaine126 <41339610+jpaine126@users.noreply.github.com>
2022-10-08 09:28:53 -0400

    cleaned up controllers and moved some logic around

--- Inverted_Pendulum_Control_Demo/controllers/lqr.py
+++ Inverted_Pendulum_Control_Demo/controllers/lqr.py
@@ -18,27 +18,20 @@ class LQR:
         self.K = K
         self.max_input = max_input
         self.max_input_on = max_input_on
-
-    def set_gain(self, K):
-        self.K = K
-
-    def max_input_enable(self):
-        self.max_input_on = True
-
-    def max_input_disable(self):
-        self.max_input_on = False
+        self.control_force = 0
 
     def update(self, states):
         """Update method to calculate force from state."""
-        control = 0
-
+        control_force = 0
         for i in range(0, len(states)):
-            control += states[i, 0] * self.K[0, i]
+            control_force += states[i, 0] * self.K[0, i]
             # print(states[i,0])
             # print(self.K[0,1])
 
         if self.max_input_on:
-            if abs(control) > self.max_input:
-                control = copysign(self.max_input, control)
+            if abs(control_force) > self.max_input:
+                control_force = copysign(self.max_input, control_force)
 
-        return -control
+        self.control_force = -control_force
+
+        return -control_force
