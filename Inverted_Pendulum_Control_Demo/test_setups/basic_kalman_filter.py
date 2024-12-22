3e6dea9564bb2361a58a22b3c4deb7c60f3a774d
jpaine126 <j.paine126@gmail.com>
2024-12-21 20:20:52 -0500

    added missing basic kalman filter test case file

---
+++ Inverted_Pendulum_Control_Demo/test_setups/basic_kalman_filter.py
@@ -0,0 +1,70 @@
+from scipy import signal
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+
+from ..core.observers import KalmanFilter
+from ..plant import PlantProtocol
+from . import ObserverTestSetup
+
+
+class BasicKalmanFilter(ObserverTestSetup, name="Basic Kalman Filter"):
+
+    params = dict(
+        Q=pd.DataFrame(
+            [
+                [0.01, 0, 0, 0],
+                [0, 0.01, 0, 0],
+                [0, 0, 0.01, 0],
+                [0, 0, 0, 0.01],
+            ],
+            index=pd.RangeIndex(0, 4, name="Q"),
+        ),
+        R=pd.DataFrame(
+            [
+                [(0.001**4) / 4, (0.001**3) / 2],
+                [(0.001**3) / 2, 0.001**2],
+            ],
+            index=pd.RangeIndex(0, 2, name="R"),
+        ),
+    )
+
+    def __init__(self, plant: PlantProtocol, sim_params, Q, R):
+        A, B, C, D = plant.linear_state_space()
+        inverse_pendulum_plant = signal.StateSpace(
+            A,
+            B,
+            C,
+            D,
+        )
+        inverse_pendulum_plant_d_kalman = inverse_pendulum_plant.to_discrete(
+            sim_params.dt_control
+        )
+
+        # Extract discrete state matrices
+        A_discrete_k = inverse_pendulum_plant_d_kalman.A
+        B_discrete_k = inverse_pendulum_plant_d_kalman.B
+        C_discrete_k = inverse_pendulum_plant_d_kalman.C
+        D_discrete_k = inverse_pendulum_plant_d_kalman.D
+
+        observer = KalmanFilter(
+            A_discrete_k,
+            B_discrete_k,
+            C_discrete_k,
+            Q,
+            R,
+        )
+        observer.x_last = np.array(
+            [
+                sim_params.x_initial,
+                sim_params.x_dot_initial,
+                sim_params.phi_initial,
+                sim_params.phi_dot_initial,
+            ]
+        ).reshape((-1, 1))
+        observer.P_last = np.eye(np.size(A, 1)) * sim_params.noise_value**2
+
+        self.observer = observer
+
+    def update(self, control_force: float, state: np.ndarray) -> np.ndarray:
+        return self.observer.update(control_force, state)
